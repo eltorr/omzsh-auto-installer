2849a43c18989c3901d1d9ea895ffd0ff9e11c69
Luis Torres <119948622+eltorr@users.noreply.github.com>
2024-12-12 17:18:50 -0600

    Update omzsh-setup.sh

--- omzsh-setup.sh
+++ omzsh-setup.sh
@@ -48,7 +48,7 @@ install_packages zsh git wget curl
 echo "Installing Oh My Zsh..."
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Check if .zshrc exists, if not, create a default one
+# Check if .zshrc exists; if not, create a default one
 if [ ! -f $HOME/.zshrc ]; then
     echo "Creating a default .zshrc from template..."
     cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
@@ -58,21 +58,33 @@ fi
 echo "Setting Zsh as the default shell..."
 chsh -s $(which zsh)
 
-# Install and configure Powerlevel10k theme
+# Install Powerlevel10k theme
 echo "Installing Powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i '/ZSH_THEME=/c\ZSH_THEME="powerlevel10k/powerlevel10k"' $HOME/.zshrc
+sed -i '/^ZSH_THEME=/c\ZSH_THEME="powerlevel10k/powerlevel10k"' $HOME/.zshrc
+
+# Disable automatic configuration wizard
+echo "Disabling automatic Powerlevel10k configuration wizard..."
+echo 'export POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >> $HOME/.zshrc
 
 # Install and configure plugins
 echo "Installing plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i '/plugins=(/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' $HOME/.zshrc
+sed -i '/^plugins=(/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' $HOME/.zshrc
 
 # Install Neofetch for system information on terminal start
 echo "Installing Neofetch..."
 install_packages neofetch
-echo "neofetch" >> $HOME/.zshrc
+if ! grep -q 'neofetch' $HOME/.zshrc; then
+    echo "neofetch" >> $HOME/.zshrc
+fi
+
+# Modify .bashrc to launch Zsh
+echo "Configuring .bashrc to launch Zsh..."
+if ! grep -q 'exec zsh' $HOME/.bashrc; then
+    echo 'exec zsh' >> $HOME/.bashrc
+fi
 
 # Apply changes and switch to Zsh
 echo "Switching to Zsh and applying changes..."
